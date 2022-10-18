d37f1db814b3a947970f28dd31bb8880505c48dd
Renan Franca <renanfranca.dev@gmail.com>
2022-10-18 12:00:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// Animação do Menu Hamburger
+
+$(document).ready(function () {
+    $(".hamburguer").click(function () {
+    $(this).toggleClass("hamburguer-active");
+    $(".nav-list-mobile").toggleClass("nav-list-mobile-active");
+    });
+});
+
+
+
+    // Slide
+
+// $(document).ready(function () {
+//     $(".hamburguer").click(function () {
+//     $(".nav-list-mobile").slideToggle(1000);
+//     });
+// });
+
+
+
+
+
---
+++ scriptSlide.js
@@ -0,0 +1,8 @@
+    // Slide
+
+$(document).ready(function () {
+    $(".hamburguer").click(function () {
+    $(".nav-list-mobile").slideToggle(1000);
+    });
+});
+
